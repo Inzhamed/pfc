e6de5509026bb978119353bc525447dc9bcb7887
sarahlkkl <sarahlkkl12@gmail.com>
2025-05-21 04:12:55 +0100

    Switch BDD to Atlas

--- backend/app/login/database.py
+++ backend/app/login/database.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017")
-db = client["railvision_db"]
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+
+if not MONGO_URI or not MONGO_DB:
+    raise ValueError("⚠️ MONGO_URI ou MONGO_DB non défini dans le .env")
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
 technicien_collection = db["techniciens"]
